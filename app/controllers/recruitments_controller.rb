aa710ff0f67b394d9242252801c8f0edb943e991
kouki135 <arumaziki2000@gmail.com>
2022-02-21 14:19:43 +0900

    recruitmentモデルにバリデーション、アソシエーション

--- app/controllers/recruitments_controller.rb
+++ app/controllers/recruitments_controller.rb
@@ -1,5 +1,18 @@
 class RecruitmentsController < ApplicationController
   def index
-    
+    @recruitments = Recruitment.new
   end
+
+  def create
+    @recruitments = Recruitment.new(recruitment_params)
+
+  end
+
+  private
+
+  def message_params
+    params.require(:recruitment).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
+
 end
--- app/models/recruitment.rb
+++ app/models/recruitment.rb
@@ -1,2 +1,9 @@
 class Recruitment < ApplicationRecord
+  validates :recruitment, presence: true
+  validates :game_title, presence: true
+  validates :content, presence: true
+
+  has_many :comments
+  belongs_to :user
+  belongs_to :favorite
 end
