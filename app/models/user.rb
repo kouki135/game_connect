a12c059c0838638b331aed9c63e1afebd468e921
kouki135 <arumaziki2000@gmail.com>
2022-02-23 23:13:18 +0900

    userモデルへフォロー機能のメソッド記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,28 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :password, presence: true, on: :create
 
-
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
+
+  has_many :relationships
+  has_many :following, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreigh_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user
+
+  def follow(other_user)
+    unless self == other_user
+      self.relationships.find_or_create_by(follow_id: other_user.id)
+    end    
+  end
+
+  def unfollow(other_user)
+    relationship = self.relationships.find_by(follow_id: other_user.id)
+    relationship.destroy if relationship
+  end
+
+  def following?(other_user)
+    self.followings.Include?(other_user)
+  end
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -23,7 +42,7 @@ class User < ApplicationRecord
   result = update_attributes(params, *options)
   clean_up_passwords
   result
-end
+  end
 
 
 end
