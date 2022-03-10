45ddae57e39e10b06d8dbd61a094c602052f388f
kouki135 <arumaziki2000@gmail.com>
2022-03-10 14:08:20 +0900

    コメント保存機能実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,16 @@
 class CommentsController < ApplicationController
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to recruitment_path(params[:recruitment_id])
+    end
+    binding.pry
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:comment).merge(user_id: current_user.id, recruitment_id: params[:recruitment_id])
+  end
+
 end
